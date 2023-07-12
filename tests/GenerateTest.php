9de096aedbf2a514aef586d02f42500e6d067cbf
Jesse Leite <jesseleite@gmail.com>
2023-07-12 19:11:03 -0400

    Add basic `ssg:generate` test.

---
+++ tests/GenerateTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests;
+
+class GenerateTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->destination = storage_path('app/static');
+
+        if ($this->files->exists($this->destination)) {
+            $this->files->deleteDirectory($this->destination);
+        }
+    }
+
+    /** @test */
+    public function it_generates_pages_for_site_fixture()
+    {
+        $files = $this->generate();
+
+        $this->assertCount(13, $files);
+
+        $this->assertStringContainsString('<h1>404!</h1>', $files['404.html']);
+
+        $this->assertStringContainsString('<h1>Page Title: Home</h1>', $files['index.html']);
+        $this->assertStringContainsString('<h1>Page Title: About</h1>', $files['about/index.html']);
+        $this->assertStringContainsString('<h1>Page Title: Topics</h1>', $files['topics/index.html']);
+
+        $this->assertStringContainsString('<h1>Articles Index Page Title</h1>', $index = $files['articles/index.html']);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/one">One</a>', $index);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/two">Two</a>', $index);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/three">Three</a>', $index);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/four">Four</a>', $index);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/five">Five</a>', $index);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/six">Six</a>', $index);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/seven">Seven</a>', $index);
+        $this->assertStringContainsString('<a href="http://cool-runnings.com/articles/eight">Eight</a>', $index);
+
+        $this->assertStringContainsString('<h1>Article Title: One</h1>', $files['articles/one/index.html']);
+        $this->assertStringContainsString('<h1>Article Title: Two</h1>', $files['articles/two/index.html']);
+        $this->assertStringContainsString('<h1>Article Title: Three</h1>', $files['articles/three/index.html']);
+        $this->assertStringContainsString('<h1>Article Title: Four</h1>', $files['articles/four/index.html']);
+        $this->assertStringContainsString('<h1>Article Title: Five</h1>', $files['articles/five/index.html']);
+        $this->assertStringContainsString('<h1>Article Title: Six</h1>', $files['articles/six/index.html']);
+        $this->assertStringContainsString('<h1>Article Title: Seven</h1>', $files['articles/seven/index.html']);
+        $this->assertStringContainsString('<h1>Article Title: Eight</h1>', $files['articles/eight/index.html']);
+    }
+
+    private function generate()
+    {
+        $this->assertFalse($this->files->exists($this->destination));
+
+        $this
+            ->artisan('statamic:ssg:generate')
+            ->doesntExpectOutputToContain('pages not generated');
+
+        $this->assertTrue($this->files->exists($this->destination));
+
+        return collect($this->files->allFiles($this->destination))
+            ->mapWithKeys(fn ($file) => [$this->relativePath($file->getPathname()) => $file->getContents()])
+            ->all();
+    }
+
+    private function relativePath($path)
+    {
+        return str_replace($this->destination.'/', '', $path);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,16 +2,20 @@
 
 namespace Tests;
 
+use Illuminate\Filesystem\Filesystem;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Statamic\Providers\StatamicServiceProvider;
 use Statamic\Statamic;
 
 class TestCase extends OrchestraTestCase
 {
+    protected $siteFixturePath = __DIR__.'/Fixtures/site';
+
     protected function getPackageProviders($app)
     {
         return [
-            StatamicServiceProvider::class,
+            \Statamic\Providers\StatamicServiceProvider::class,
+            \Wilderborn\Partyline\ServiceProvider::class,
+            \Statamic\StaticSite\ServiceProvider::class,
         ];
     }
 
@@ -20,5 +24,55 @@ class TestCase extends OrchestraTestCase
         return [
             'Statamic' => Statamic::class,
         ];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->files = app(Filesystem::class);
+
+        $this->copyDirectoryFromFixture('content');
+        $this->copyDirectoryFromFixture('resources');
+    }
+
+    protected function copyDirectoryFromFixture($directory)
+    {
+        if (base_path($directory)) {
+            $this->files->deleteDirectory(base_path($directory));
+        }
+
+        $this->files->copyDirectory("{$this->siteFixturePath}/{$directory}", base_path($directory));
+    }
+
+    protected function resolveApplicationConfiguration($app)
+    {
+        parent::resolveApplicationConfiguration($app);
+
+        $configs = [
+            'assets', 'cp', 'forms', 'routes', 'static_caching',
+            'sites', 'stache', 'system', 'users',
+        ];
+
+        foreach ($configs as $config) {
+            $app['config']->set("statamic.$config", require(__DIR__."/../vendor/statamic/cms/config/{$config}.php"));
+        }
+
+        $files = new Filesystem;
+
+        $files->copyDirectory(__DIR__.'/../vendor/statamic/cms/config', config_path('statamic'));
+
+        $configs = [
+            'filesystems',
+            'statamic/users',
+            'statamic/stache',
+            'statamic/sites',
+        ];
+
+        foreach ($configs as $config) {
+            $files->delete(config_path("{$config}.php"));
+            $files->copy("{$this->siteFixturePath}/config/{$config}.php", config_path("{$config}.php"));
+            $app['config']->set(str_replace('/', '.', $config), require("{$this->siteFixturePath}/config/{$config}.php"));
+        }
     }
 }
