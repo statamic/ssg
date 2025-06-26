e0a448259488e4dc2a86364ace4d1d822366e2e0
Jesse Leite <jesseleite@gmail.com>
2025-06-26 15:24:30 -0400

    Add config to enforce trailing slashes during generation.

--- config/ssg.php
+++ config/ssg.php
@@ -75,6 +75,21 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Trailing Slashes
+    |--------------------------------------------------------------------------
+    |
+    | Enable to enforce trailing slashes on generated URLs. Some static site
+    | hosts expect trailing slashes on URLs for things like SEO sitemaps
+    | and RSS feeds in order to comply with the server configuration.
+    |
+    | NOTE: This is an experimental feature, and requires Statamic 6+!
+    |
+    */
+
+    'enforce_trailing_slashes' => false,
+
+    /*
+    |--------------------------------------------------------------------------
     | Pagination Route
     |--------------------------------------------------------------------------
     |
